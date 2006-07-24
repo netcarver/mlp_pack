d333196e6febc928c3f178382eeb32f81cd2343a
graeme <graeme@rgbp.co.uk>
2006-07-24 19:05:05 +0000

    SVN keywords are working, here they are for the other files.

--- gbp_l10n.php
+++ gbp_l10n.php
@@ -7,6 +7,10 @@ $plugin['author_uri'] = 'http://porteo.us/projects/textpattern/gbp_l10n/';
 $plugin['description'] = 'Textpattern content localisation.';
 $plugin['type'] = '1';
 
+$plugin['url'] = '$HeadURL$';
+$plugin['date'] = '$LastChangedDate$';
+$plugin['revision'] = '$LastChangedRevision$';
+
 @include_once('../zem_tpl.php');
 
 if (0) {
@@ -101,7 +105,7 @@ class LocalisationView extends GBPPlugin
 
 		'plugins'	=> array('value' => 1, 'type' => 'yesnoradio'),
 		
-		'inline_editing' => array('value' => 0, 'type' => 'yesnoradio'),
+		'inline.editing' => array('value' => 0, 'type' => 'yesnoradio'),
 		);
 
 	var $strings_lang = 'en-gb';
@@ -116,7 +120,7 @@ class LocalisationView extends GBPPlugin
 	'gbp_l10n_edit_resource'		=> 'Edit $type: $owner ',
 	'gbp_l10n_explain_extra_lang'	=> '<p>* These languages are not specified in the site preferences.</p><p>If they are not needed for your site you can delete them.</p>',
 	'languages' 					=> 'Languages ',
-	'inline_editing'				=> 'Inline editing of pages and forms ',
+	'inline.editing'				=> 'Inline editing of pages and forms ',
 	'gbp_l10n_lang_remove_warning'	=> 'This will remove ALL plugin strings/snippets in $var1. ',
 	'gbp_l10n_localised'			=> 'Localised',
 	'gbp_l10n_missing'				=> ' missing.', 
