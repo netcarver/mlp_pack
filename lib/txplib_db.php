38e449604b25ebde3d2d203ee597d7ae7b800c98
netcarver <nc.github@netcarving.com>
2006-09-05 07:22:55 +0000

    Added an alias for the 'textpattern' table to allow public side routines to access 'textpattern' via the alias and avoid the language mapper. This is needed to support public side submission of comments which needs to update both the language-specific articles table and the master table too.

--- lib/txplib_db.php
+++ lib/txplib_db.php
@@ -1,6 +1,8 @@
 <?php
 
 /*
+$HeadURL: http://svn.textpattern.com/development/4.0/textpattern/lib/txplib_db.php $
+$LastChangedRevision: 1686 $
 */
 
 if (!defined('PFX')) {
@@ -12,12 +14,12 @@ if (!defined('PFX')) {
 set_magic_quotes_runtime(0);
 
 class DB {
-    function DB()
+	function DB()
 	{
 		global $txpcfg;
 
 		$this->host = $txpcfg['host'];
-		$this->db   = $txpcfg['db'];
+		$this->db	= $txpcfg['db'];
 		$this->user = $txpcfg['user'];
 		$this->pass = $txpcfg['pass'];
 
@@ -35,7 +37,7 @@ class DB {
 		// be backwardscompatible
 		if ( isset($txpcfg['dbcharset']) && (intval($version[0]) >= 5 || preg_match('#^4\.[1-9]#',$version)) )
 			mysql_query("SET NAMES ". $txpcfg['dbcharset']);
-    }
+	}
 }
 $DB = new DB;
 
@@ -44,17 +46,24 @@ $DB = new DB;
 	function _redirect_textpattern($table)
 		{
 		# Only redirect public interface calls to the textpattern table...
-		if( @txpinterface==='public' and 'textpattern' === $table )
+		if( @txpinterface==='public' )
 			{
-			global $gbp_language;
-
-			$language_set 	= isset( $gbp_language );
-			$language_ok	= true;
-			if( $language_set and $language_ok )
+			if( 'textpattern' === $table )
 				{
-				//echo br , "_redirect_textpattern($table) to ... ";
-				$table = $table.'_'.$gbp_language['short'];
-				//echo $table;
+				global $gbp_language;
+
+				$language_set 	= isset( $gbp_language );
+				$language_ok	= true;
+				if( $language_set and $language_ok )
+					{
+					//echo br , "_redirect_textpattern($table) to ... ";
+					$table = $table.'_'.$gbp_language['short'];
+					//echo $table;
+					}
+				}
+			elseif( 'l10n_master_textpattern' === $table )
+				{
+				$table = 'textpattern';
 				}
 			}
 		return $table;
@@ -90,7 +99,7 @@ $DB = new DB;
 		global $DB,$txpcfg, $qcount, $qtime, $production_status;
 		$method = (!$unbuf) ? 'mysql_query' : 'mysql_unbuffered_query';
 		if (!$q) return false;
-		if ($debug or TXP_DEBUG === 1) {
+		if ($debug or TXP_DEBUG === 1) { 
 			dmp($q);
 			dmp(mysql_error());
 //			dmp(debug_backtrace());
@@ -100,8 +109,8 @@ $DB = new DB;
 		$time = sprintf('%02.6f', getmicrotime() - $start);
 		@$qtime += $time;
 		@$qcount++;
-		if ($result === false and (@$production_status == 'debug' or @$production_status == 'test')) {
-			$caller = ($production_status == 'test') ? n . join("\n", get_caller()) : '';
+		if ($result === false and (@$production_status == 'debug' or @$production_status == 'testing')) {
+			$caller = ($production_status == 'debug') ? n . join("\n", get_caller()) : '';
 			trigger_error(mysql_error() . n . $q . $caller, E_USER_WARNING);
 		}
 
@@ -122,7 +131,7 @@ $DB = new DB;
 	}
 
 // -------------------------------------------------------------
-	function safe_update($table, $set, $where, $debug='')
+	function safe_update($table, $set, $where, $debug='') 
 	{
 		$q = "update ".safe_pfx($table)." set $set where $where";
 		if ($r = safe_query($q,$debug)) {
@@ -132,7 +141,7 @@ $DB = new DB;
 	}
 
 // -------------------------------------------------------------
-	function safe_insert($table,$set,$debug='')
+	function safe_insert($table,$set,$debug='') 
 	{
 		global $DB;
 		$q = "insert into ".safe_pfx($table)." set $set";
@@ -145,7 +154,7 @@ $DB = new DB;
 
 // -------------------------------------------------------------
 // insert or update
-	function safe_upsert($table,$set,$where,$debug='')
+	function safe_upsert($table,$set,$where,$debug='') 
 	{
 		// FIXME: lock the table so this is atomic?
 		$r = safe_update($table, $set, $where, $debug);
@@ -156,7 +165,7 @@ $DB = new DB;
 	}
 
 // -------------------------------------------------------------
-	function safe_alter($table, $alter, $debug='')
+	function safe_alter($table, $alter, $debug='') 
 	{
 		$q = "alter table ".safe_pfx($table)." $alter";
 		if ($r = safe_query($q,$debug)) {
@@ -166,7 +175,7 @@ $DB = new DB;
 	}
 
 // -------------------------------------------------------------
-	function safe_optimize($table, $debug='')
+	function safe_optimize($table, $debug='') 
 	{
 		$q = "optimize table ".safe_pfx($table)."";
 		if ($r = safe_query($q,$debug)) {
@@ -176,7 +185,7 @@ $DB = new DB;
 	}
 
 // -------------------------------------------------------------
-	function safe_repair($table, $debug='')
+	function safe_repair($table, $debug='') 
 	{
 		$q = "repair table ".safe_pfx($table)."";
 		if ($r = safe_query($q,$debug)) {
@@ -186,7 +195,7 @@ $DB = new DB;
 	}
 
 // -------------------------------------------------------------
-	function safe_field($thing, $table, $where, $debug='')
+	function safe_field($thing, $table, $where, $debug='') 
 	{
 		$q = "select $thing from ".safe_pfx_j($table)." where $where";
 		$r = safe_query($q,$debug);
@@ -199,7 +208,7 @@ $DB = new DB;
 	}
 
 // -------------------------------------------------------------
-	function safe_column($thing, $table, $where, $debug='')
+	function safe_column($thing, $table, $where, $debug='') 
 	{
 		$q = "select $thing from ".safe_pfx_j($table)." where $where";
 		$rs = getRows($q,$debug);
@@ -214,7 +223,7 @@ $DB = new DB;
 	}
 
 // -------------------------------------------------------------
-	function safe_row($things, $table, $where, $debug='')
+	function safe_row($things, $table, $where, $debug='') 
 	{
 		$q = "select $things from ".safe_pfx_j($table)." where $where";
 		$rs = getRow($q,$debug);
@@ -226,7 +235,7 @@ $DB = new DB;
 
 
 // -------------------------------------------------------------
-	function safe_rows($things, $table, $where, $debug='')
+	function safe_rows($things, $table, $where, $debug='') 
 	{
 		$q = "select $things from ".safe_pfx_j($table)." where $where";
 		$rs = getRows($q,$debug);
@@ -237,20 +246,20 @@ $DB = new DB;
 	}
 
 // -------------------------------------------------------------
-	function safe_rows_start($things, $table, $where, $debug='')
+	function safe_rows_start($things, $table, $where, $debug='') 
 	{
 		$q = "select $things from ".safe_pfx_j($table)." where $where";
 		return startRows($q,$debug);
 	}
 
 //-------------------------------------------------------------
-	function safe_count($table, $where, $debug='')
+	function safe_count($table, $where, $debug='') 
 	{
 		return getThing("select count(*) from ".safe_pfx_j($table)." where $where",$debug);
 	}
 
 // -------------------------------------------------------------
-	function safe_show($thing, $table, $debug='')
+	function safe_show($thing, $table, $debug='') 
 	{
 		$q = "show $thing from ".safe_pfx($table)."";
 		$rs = getRows($q,$debug);
@@ -262,7 +271,7 @@ $DB = new DB;
 
 
 //-------------------------------------------------------------
-	function fetch($col,$table,$key,$val,$debug='')
+	function fetch($col,$table,$key,$val,$debug='') 
 	{
 		$q = "select $col from ".safe_pfx($table)." where `$key` = '$val' limit 1";
 		if ($r = safe_query($q,$debug)) {
@@ -274,7 +283,7 @@ $DB = new DB;
 	}
 
 //-------------------------------------------------------------
-	function getRow($query,$debug='')
+	function getRow($query,$debug='') 
 	{
 		if ($r = safe_query($query,$debug)) {
 			$row = (mysql_num_rows($r) > 0) ? mysql_fetch_assoc($r) : false;
@@ -285,11 +294,11 @@ $DB = new DB;
 	}
 
 //-------------------------------------------------------------
-	function getRows($query,$debug='')
+	function getRows($query,$debug='') 
 	{
 		if ($r = safe_query($query,$debug)) {
 			if (mysql_num_rows($r) > 0) {
-				while ($a = mysql_fetch_assoc($r)) $out[] = $a;
+				while ($a = mysql_fetch_assoc($r)) $out[] = $a; 
 				mysql_free_result($r);
 				return $out;
 			}
@@ -319,7 +328,7 @@ $DB = new DB;
 	}
 
 //-------------------------------------------------------------
-	function getThing($query,$debug='')
+	function getThing($query,$debug='') 
 	{
 		if ($r = safe_query($query,$debug)) {
 			$thing = (mysql_num_rows($r) != 0) ? mysql_result($r,0) : '';
@@ -330,7 +339,7 @@ $DB = new DB;
 	}
 
 //-------------------------------------------------------------
-	function getThings($query,$debug='')
+	function getThings($query,$debug='') 
 	// return values of one column from multiple rows in an num indexed array
 	{
 		$rs = getRows($query,$debug);
@@ -340,98 +349,96 @@ $DB = new DB;
 		}
 		return array();
 	}
-
+	
 //-------------------------------------------------------------
-	function getCount($table,$where,$debug='')
+	function getCount($table,$where,$debug='') 
 	{
 		return getThing("select count(*) from ".safe_pfx_j($table)." where $where",$debug);
 	}
 
 // -------------------------------------------------------------
- 	function getTree($root, $type)
- 	{
+	function getTree($root, $type, $where='1=1')
+	{
 
 		$root = doSlash($root);
 		$type = doSlash($type);
 
-	   $rs = safe_row(
-	    	"lft as l, rgt as r",
-	    	"txp_category",
+		$rs = safe_row(
+			"lft as l, rgt as r",
+			"txp_category",
 			"name='$root' and type = '$type'"
 		);
 
 		if (!$rs) return array();
 		extract($rs);
 
-		$inc_root = ($root == 'root') ? " and name != 'root'" : '';
-
 		$out = array();
-		$right = array();
+		$right = array(); 
 
-	    $rs = safe_rows_start(
-	    	"id, name, lft, rgt, parent, title",
-	    	"txp_category",
-	    	"lft between $l and $r and type = '$type' $inc_root order by lft asc"
-		);
+		$rs = safe_rows_start(
+			"id, name, lft, rgt, parent, title",
+			"txp_category",
+			"lft between $l and $r and type = '$type' and name != 'root' and $where order by lft asc"
+		); 
 
-	    while ($rs and $row = nextRow($rs)) {
-	   		extract($row);
+		while ($rs and $row = nextRow($rs)) {
+			extract($row);
 			while (count($right) > 0 && $right[count($right)-1] < $rgt) {
 				array_pop($right);
 			}
 
-        	$out[] =
-        		array(
-        			'id' => $id,
-        			'name' => $name,
-        			'title' => $title,
-        			'level' => count($right),
-        			'children' => ($rgt - $lft - 1) / 2
-        		);
+			$out[] =
+				array(
+					'id' => $id,
+					'name' => $name,
+					'title' => $title,
+					'level' => count($right),
+					'children' => ($rgt - $lft - 1) / 2
+				);
 
-	        $right[] = $rgt;
-	    }
-    	return($out);
- 	}
+			$right[] = $rgt;
+		}
+		return($out);
+	}
 
 // -------------------------------------------------------------
- 	function getTreePath($target, $type)
- 	{
+	function getTreePath($target, $type)
+	{
 
-	  	$rs = safe_row(
-	    	"lft as l, rgt as r",
-	    	"txp_category",
+		$rs = safe_row(
+			"lft as l, rgt as r",
+			"txp_category",
 			"name='".doSlash($target)."' and type = '".doSlash($type)."'"
 		);
 		if (!$rs) return array();
 		extract($rs);
 
-	    $rs = safe_rows_start(
-	    	"*",
-	    	"txp_category",
+		$rs = safe_rows_start(
+			"*",
+			"txp_category",
 				"lft <= $l and rgt >= $r and type = '".doSlash($type)."' order by lft asc"
 		);
 
 		$out = array();
 		$right = array();
 
-	    while ($rs and $row = nextRow($rs)) {
-	   		extract($row);
+		while ($rs and $row = nextRow($rs)) {
+			extract($row);
 			while (count($right) > 0 && $right[count($right)-1] < $rgt) {
 				array_pop($right);
 			}
 
-        	$out[] =
-        		array(
-        			'id' => $id,
-        			'name' => $name,
-        			'title' => $title,
-        			'level' => count($right),
-        			'children' => ($rgt - $lft - 1) / 2
-        		);
+			$out[] =
+				array(
+					'id' => $id,
+					'name' => $name,
+					'title' => $title,
+					'level' => count($right),
+					'children' => ($rgt - $lft - 1) / 2
+				);
 
-	        $right[] = $rgt;
-	    }
+			$right[] = $rgt;
+		}
 		return $out;
 	}
 
@@ -446,16 +453,16 @@ $DB = new DB;
 			"parent='$parent' and type='$type' order by name");
 
 		foreach($result as $row) {
-    	    $right = rebuild_tree($row, $right, $type);
-	    }
+			$right = rebuild_tree($row, $right, $type);
+		}
 
-	    safe_update(
-	    	"txp_category",
-	    	"lft=$left, rgt=$right",
-	    	"name='$parent' and type='$type'"
-	    );
-    	return $right+1;
- 	}
+		safe_update(
+			"txp_category",
+			"lft=$left, rgt=$right",
+			"name='$parent' and type='$type'"
+		);
+		return $right+1;
+	}
 
 //-------------------------------------------------------------
 	function get_prefs()
