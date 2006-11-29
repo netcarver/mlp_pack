cf4b3f099c5a04b62729a9ab77549b7daa1ccc26
netcarver <nc.github@netcarving.com>
2006-11-29 10:37:43 +0000

    * Finally optimised loading/registration of strings. Now admin-side only even though this is a public plugin.

--- zem_contact_lang-mlp.php
+++ zem_contact_lang-mlp.php
@@ -15,10 +15,6 @@ $plugin['revision'] = '$LastChangedRevision$';
 
 if (0) {
 ?>
-<!-- CSS SECTION
-# --- BEGIN PLUGIN CSS ---
-# --- END PLUGIN CSS ---
--->
 <!-- HELP SECTION
 # --- BEGIN PLUGIN HELP ---
 
@@ -45,13 +41,16 @@ If you have a dual-language site and the languages use separate &#8220;sections&
 }
 
 # --- BEGIN PLUGIN CODE ---
-
+#
 #	Define a (hopefully) unique prefix for our strings...
+#
 if( !defined( 'ZCRL_PREFIX' ) )
 	define( 'ZCRL_PREFIX' , 'zem_crl' );
 
+#
 # 	Here are the strings. Note that they all use the single quote format.
 # The variable substitutions will be done as the strings are used.
+#
 global $zem_crl_strings;
 $zem_crl_strings = array(
 	'checkbox'			=> 'Checkbox',
@@ -84,27 +83,29 @@ $zem_crl_strings = array(
 	);
 
 
+
+#
 #	Register the callback for the enumerate string event.
 # If the MLP pack is not present and active this will NOT get called.
-if( @txpinterface=='public' )
+#
+register_callback( 'zem_crl_enumerate_strings' , 'l10n.enumerate_strings' );
+
+
+#
+#	Here's a callback routine used to register the above strings with
+# the MLP Pack (if installed).
+#
+function zem_crl_enumerate_strings($event , $step='' , $pre=0)
 	{
-	register_callback( 'zem_crl_enumerate_strings' , 'l10n.enumerate_strings' );
-
-	#	Here's a callback routine used to register the above strings with
-	# the MLP Pack (if installed).
-	function zem_crl_enumerate_strings($event , $step='' , $pre=0)
-		{
-		global $zem_crl_strings;
-		$r = array	(
-					'owner'		=> 'zem_contact_lang_mlp',	#	Name the plugin these strings are for.
-					'prefix'	=> ZCRL_PREFIX,				#	Its unique string prefix
-					'lang'		=> 'en-gb',					#	The language of the initial strings.
-					'event'		=> 'public',				#	public/admin/common = which interface the strings will be loaded into
-					'strings'	=> $zem_crl_strings,		#	The strings themselves.
-					);
-		return $r;
-		}
-
+	global $zem_crl_strings;
+	$r = array	(
+				'owner'		=> 'zem_contact_lang_mlp',	#	Name the plugin these strings are for.
+				'prefix'	=> ZCRL_PREFIX,				#	Its unique string prefix
+				'lang'		=> 'en-gb',					#	The language of the initial strings.
+				'event'		=> 'public',				#	public/admin/common = which interface the strings will be loaded into
+				'strings'	=> $zem_crl_strings,		#	The strings themselves.
+				);
+	return $r;
 	}
 
 #
@@ -112,42 +113,56 @@ if( @txpinterface=='public' )
 #
 #	Need to make this fallback to the local array in case this is not being used with the MLP pack.
 #
-function zem_contact_gTxt( $what , $var1='' , $var2='' )
+if( @txpinterface=='public' )
 	{
-	global $textarray;
-	global $zem_crl_strings;
-
-	#	Build an array of substitutions...
-	$args = array();
-	if( !empty( $var1 ) )
-		$args['$var1'] = $var1;
-	if( !empty( $var2 ) )
-		$args['$var2'] = $var2;
-
-	#	Prepare the prefixed key for use...
-	$key = ZCRL_PREFIX . '-' . $what;
-	$key = strtolower($key);
-
-	#	Grab from the global textarray (possibly edited by MLP) if we can...
-	if(isset($textarray[$key]))
+	function zem_contact_gTxt( $what , $var1='' , $var2='' )
 		{
-		$str = $textarray[$key];
-		}
-	else
-		{
-		#	Use the non-preficed key...
-		$key = strtolower($what);
+		global $textarray;
+		global $zem_crl_strings;
 
-		#	Grab from the internal array if possible...
-		if( isset( $zem_crl_strings[$key] ) )
-			$str = $zem_crl_strings[$key];
+		#
+		#	Build an array of substitutions...
+		#
+		$args = array();
+		if( !empty( $var1 ) )
+			$args['$var1'] = $var1;
+		if( !empty( $var2 ) )
+			$args['$var2'] = $var2;
+
+		#
+		#	Prepare the prefixed key for use...
+		#
+		$key = ZCRL_PREFIX . '-' . $what;
+		$key = strtolower($key);
+
+		#
+		#	Grab from the global textarray (possibly edited by MLP) if we can...
+		#
+		if(isset($textarray[$key]))
+			{
+			$str = $textarray[$key];
+			}
 		else
-			#	Fallback to returning the key if not present...
-			$str = $what;
-		}
-	$str = strtr( $str , $args );
-	return $str;
-	}
+			{
+			#
+			#	Use the non-preficed key...
+			#
+			$key = strtolower($what);
 
+			#
+			#	Grab from the internal array if possible...
+			#
+			if( isset( $zem_crl_strings[$key] ) )
+				$str = $zem_crl_strings[$key];
+			else
+				#
+				#	Fallback to returning the key if not present...
+				#
+				$str = $what;
+			}
+		$str = strtr( $str , $args );
+		return $str;
+		}
+	}
 # --- END PLUGIN CODE ---
 ?>
