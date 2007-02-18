e4ae2db1e2eb703be003a369a7b7b57f37450072
netcarver <nc.github@netcarving.com>
2007-02-18 00:23:51 +0000

    * New string for no-privs on DB condition.

--- lib/l10n_el-gr_strings.php
+++ lib/l10n_el-gr_strings.php
@@ -35,6 +35,7 @@ $l10n_default_strings_perm = array(
 	'l10n-op_tables'      => '{op} the {tables} tables',
 	'l10n-comment_op'     => '{op} the default comment invitation',
 	'l10n-missing_thing'  => 'missing {thing}',
+	'l10n-missing_all_privs' => ' privs - or grants on DB (`{db}` as `{escaped_db}`) cannot be evaluated',
 	'l10n-add_field'      => 'Add the `{table}.{field}` field',
 	'l10n-skip_field'     => 'Skip the `{table}.{field}` field - it already exists',
 	'l10n-copy_defaults'  => 'Copy defaults to `{table}.{field}` field',
